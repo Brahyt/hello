763da17192091b3a217fb515f2d20139f0ee1f37
Daniel Bright <brahyt@gmail.com>
2020-03-16 08:12:28 -0700

    handle connection;

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -7,4 +10,11 @@ fn main() {
 
         println!("Connection established");
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
